0616052c3918ad7a190f898d8f4ae0bec184fffb
seguna15 <seguna15@gmail.com>
2023-08-20 06:57:11 +0100

    changes to cookies

--- apps/auth/src/auth.service.ts
+++ apps/auth/src/auth.service.ts
@@ -49,8 +49,6 @@ export class AuthService {
      response.cookie('Authentication', token, {
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000,  
-      secure: true,
-      sameSite: 'none',
     }); //sameSite: 'none', secure: true, for production
 
     return token;
